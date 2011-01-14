5d686f56cb0f8e0845a18da562227ac3eed371e5
uly55e5 <uly55e5@web.de>
2011-01-14 11:34:50 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChemDB::Application.config.secret_token = '26845859c4da8bb8a36b7f1973cc339d0d7c4aec81a07a687b3b98208531361d981c40078caa988f9e07201cb5b1c88334721e2b4c70b903f0162131ab47d2ea'
